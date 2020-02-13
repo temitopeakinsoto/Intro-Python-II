ed16efaef1d9af0c7cc7d84422fc3c7a8a81ac4b
Temitope Akinsoto <takinsoto@gmail.com>
2020-02-13 13:55:42 +0100

    Write a player class to hold info about player room

--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, player_room):
+        self.player_room  = player_room
+    
+    def __str__(self):
+        return f"player in room {self.player_room}"
+
+
+player = Player("Lagos Room")
+print(player)
+
